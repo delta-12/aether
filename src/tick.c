76b02a06be7f060345c9b699c2171f142b88b021
delta-12 <btesq235@gmail.com>
2025-12-29 13:14:15 -0500

    user tick and tick tests

--- src/tick.c
+++ src/tick.c
@@ -1,9 +1,29 @@
 #include "tick.h"
 
+#ifndef AETHER_TICK_USER
+#include <time.h>
+
+#define A_TICK_MILLISECONDS_TO_SECONDS     1000U
+#define A_TICK_NANOSECONDS_TO_MILLISECONDS 1000000U
+#else
+extern a_Tick_Ms_t a_TickUser_GetTick(void);
+#endif /* AETHER_TICK_USER */
+
 a_Tick_Ms_t a_Tick_GetTick(void)
 {
-    /* TODO */
-    return 0U;
+#ifdef AETHER_TICK_USER
+    return a_TickUser_GetTick();
+#else
+    a_Tick_Ms_t     tick = A_TICK_MS_MAX;
+    struct timespec time;
+
+    if (0 != timespec_get(&time, TIME_UTC))
+    {
+        tick = (a_Tick_Ms_t)((int64_t)time.tv_sec * A_TICK_MILLISECONDS_TO_SECONDS + (int64_t)time.tv_nsec / A_TICK_NANOSECONDS_TO_MILLISECONDS);
+    }
+
+    return tick;
+#endif /* AETHER_TICK_USER */
 }
 
 a_Tick_Ms_t a_Tick_GetElapsed(const a_Tick_Ms_t start)
